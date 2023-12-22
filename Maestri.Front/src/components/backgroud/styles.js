01f3f2eb9ba99f3f72ad03ddbd1fc00b1a045ffc
LuizRissardi <rissardi.luiz2006@gmail.com>
2023-12-22 18:07:51 -0300

    starting a project in React Native and initializing the initial registration and login screen part

---
+++ Maestri.Front/src/components/backgroud/styles.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+import colors from "../../../design-System/global-styles";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    position: "absolute",
+  },
+  gradientOverlay: {
+    flex: 1,
+    width: "100%",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 150,
+    borderBottomWidth: 30,
+    borderBottomColor: "rgba(175, 202, 235, 0)",
+  },
+  mask: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0, 0, 0, 0.5)", // Cor e opacidade da máscara
+  },
+});
+
+export default styles;
